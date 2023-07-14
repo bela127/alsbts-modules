16cd9911c9401b6e6ab4b250502ff0eb79bfba01
bela127 <bhb127@outlook.de>
2023-07-14 12:39:26 +0200

    query_selector update to new subscriber names and interface

--- alsbts/modules/query/query_selector.py
+++ alsbts/modules/query/query_selector.py
@@ -1,12 +1,17 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
 
 from alts.core.query.query_selector import QuerySelector
 
+if TYPE_CHECKING:
+    from alts.core.subscribable import Subscribable
+
 class StreamQuerySelector(QuerySelector):
     
-    def stream_update(self):
+    def stream_update(self, subscription: Subscribable):
         self.decide()
-        return super().stream_update()
+        return super().stream_update(subscription)
 
-    def update(self) -> None:
-        return super().update()
+    def update(self, subscription: Subscribable) -> None:
+        return super().update(subscription)
 
